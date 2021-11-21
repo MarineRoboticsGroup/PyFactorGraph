75a839e038ebf19514790115b91377b6563ed537
Jospeh <jrales@mit.edu>
2021-11-21 17:46:10 -0500

    Add optional timestamps to the variables, priors, and measurements

--- py_factor_graph/variables.py
+++ py_factor_graph/variables.py
@@ -1,5 +1,5 @@
 import attr
-from typing import Tuple
+from typing import Tuple, Optional
 import numpy as np
 
 
@@ -11,11 +11,13 @@ class PoseVariable:
         name (str): the name of the variable (defines the frame)
         true_position (Tuple[float, float]): the true position of the robot
         true_theta (float): the true orientation of the robot
+        timestamp (float): seconds since epoch
     """
 
     name: str = attr.ib()
     true_position: Tuple[float, float] = attr.ib()
     true_theta: float = attr.ib()
+    timestamp: Optional[float] = attr.ib(default=None)
 
     @property
     def rotation_matrix(self):
