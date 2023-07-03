fix covariance getter in PosePrior classes

py_factor_graph/priors.py
@@ -9,6 +9,7 @@ from py_factor_graph.utils.attrib_utils import (
     positive_float_validator,
 )
 from py_factor_graph.utils.matrix_utils import (
+    get_covariance_matrix_from_measurement_precisions,
     get_rotation_matrix_from_theta,
     get_quat_from_rotation_matrix,
 )
@@ -51,12 +52,8 @@ class PosePrior2D:
 
     @property
     def covariance(self):
-        return np.diag(
-            [
-                1 / self.translation_precision,
-                1 / self.translation_precision,
-                1 / self.rotation_precision,
-            ]
+        return get_covariance_matrix_from_measurement_precisions(
+            self.translation_precision, self.rotation_precision, mat_dim=3
         )
 
 
@@ -91,15 +88,8 @@ class PosePrior3D:
 
     @property
     def covariance(self) -> np.ndarray:
-        return np.diag(
-            [
-                1 / self.translation_precision,
-                1 / self.translation_precision,
-                1 / self.translation_precision,
-                1 / self.rotation_precision,
-                1 / self.rotation_precision,
-                1 / self.rotation_precision,
-            ]
+        return get_covariance_matrix_from_measurement_precisions(
+            self.translation_precision, self.rotation_precision, mat_dim=6
         )
 
     @property